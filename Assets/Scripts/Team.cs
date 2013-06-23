83c6f755feff513ab02199da1cf575651f8467f5
Salvatore Lopiparo <toreltwiddler@gmail.com>
2013-06-23 16:33:54 -0700

    Team scores working

--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -6,12 +6,15 @@ public class Team : MonoBehaviour {
 	public float points = 0.0f;
 	public GameObject scoreBar;
 	public GameObject colorBar;
+	public float barHeight = 5;
 	
 	private float totalPoints;
 	private Color color = Color.white;
+	private int numberOfTeams;
 	
 	void Awake () {
 		totalPoints = GameObject.Find("LevelBuilder").GetComponent<LevelBuilder>().totalPoints;
+		numberOfTeams = GameObject.Find("LevelBuilder").GetComponent<LevelBuilder>().numberOfTeams;
 	}
 
 	// Use this for initialization
@@ -35,7 +38,7 @@ public class Team : MonoBehaviour {
 	
 	public void GivePoints(int points_given) {
 		points += points_given;
-		scoreBar.transform.localScale = new Vector3((points / totalPoints) * 20, 1, 1);
+		scoreBar.transform.localScale = new Vector3((points / (totalPoints / numberOfTeams)) * barHeight, 1, 1);
 		if (points >= totalPoints / 2) {
 			GameObject.Find("SceneManager").GetComponent<SceneManager>().LoadMainMenu();
 		}
