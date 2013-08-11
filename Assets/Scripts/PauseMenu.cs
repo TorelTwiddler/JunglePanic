f39e0530fb26bc6d519c023397f0e059db4e748c
Topher <hughestopher86@gmail.com>
2013-08-11 16:17:11 -0700

    changed menu asset materials to unlit

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			TogglePause();
+		}
+	}
+	
+	private void TogglePause(){
+		if(Time.timeScale == 1.0f){
+			print("pause");
+			Time.timeScale = 0.0f;
+			Time.fixedDeltaTime = 0.0f;
+		}
+		else{
+			print("unpause");
+			Time.timeScale = 1.0f;
+			Time.fixedDeltaTime = 1.0f;
+		}
+	}
+}
